426d846a1e9b6da65c0967da6aeddfea667a854c
aammddkkzxc <ideal0427@naver.com>
2024-09-22 22:34:16 +0900

    feat: 트랜잭션 전파 활용4 - 전파 커밋

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -46,4 +46,15 @@ class MemberServiceTest {
         assertTrue(logRepository.find(username).isEmpty());
     }
 
+    @Test
+    @DisplayName("트랜잭션 전파 예제 모두 같은 커넥션 사용")
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
+        //when
+        memberService.joinV1(username);
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
 }
