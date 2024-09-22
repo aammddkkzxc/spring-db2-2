2c2ec7c90d821d05ac0307f05c1684812b1f1720
aammddkkzxc <ideal0427@naver.com>
2024-09-22 21:32:27 +0900

    feat: 트랜잭션 전파 활용1 - 예제 프로젝트 시작

---
+++ src/main/java/hello/springtx/propagation/MemberService.java
@@ -0,0 +1,39 @@
+package hello.springtx.propagation;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+    private final LogRepository logRepository;
+
+    public void joinV1(String username) {
+
+        Member member = new Member(username);
+        Log log = new Log(username);
+
+        memberRepository.save(member);
+        logRepository.save(log);
+
+    }
+
+    public void joinV2(String username) {
+
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        memberRepository.save(member);
+        try {
+            logRepository.save(logMessage);
+        } catch (IllegalStateException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름으로 전환 예시");
+        }
+    }
+
+}
